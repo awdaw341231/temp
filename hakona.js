2a0770b0a8769418e99d05e37a1e46b577d5cbd0
awdaw341231 <eahned08@gmail.com>
2024-10-20 19:02:57 +0300

    Create hakona.js

---
+++ hakona.js
@@ -0,0 +1,2 @@
+document.body.style.backgroundImage = "url('https://i.imgur.com/5rZ91h5.gif')";
+document.getElementsByClassName("row")[0].innerHTML += "<h1 style='color: white;'> 🇵🇸 From Gaza With Love 🇵🇸</h1>";
